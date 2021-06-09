03f83d49efbe778feaaadab3c67f9dbf107c9a62
sushen <sushenbiswasaga@gmail.com>
2021-06-09 09:58:34 +0600

    Gui Button

--- Gui.py
+++ Gui.py
@@ -2,8 +2,12 @@ from tkinter import *
 
 root = Tk()
 
-myLebel =Label(root, text = "Hello World")
+# myLebel1 =Label(root, text = "Hello World").grid(row = 0,column=0)
+# myLebel2 =Label(root, text = "My nameis Sushen Biswas").grid(row = 1,column=5)
 
-myLebel.pack()
+myButton = Button(root, text="Click Me!")
+myButton.pack()
+# myLebel1.grid(row = 0,column=0)
+# myLebel2.grid(row = 1,column=5)
 
 root.mainloop()
